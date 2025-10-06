0a3999bd234ad4f52ca0ff826007df7c4b172fa1
Shubham Mahajan <mhajan.shubh@gmail.com>
2025-10-06 21:08:16 +0530

    Started Coding Practices

--- TrialPage.cpp
+++ TrialPage.cpp
@@ -1,39 +1,7 @@
-#include <bits/stdc++.h>
-// #include <iostream>
-// #include<algorithm>
-// #include<climits>
-// #include<string>
-// #include<cctype>
-// #include<vector>
-// #include<set>
-// #include<iomanip>
-// #include<cmath>
-// #include<list>
-// #include<iterator>
+#include<bits/stdc++.h>
 using namespace std;
-
-bool isprime(int n)
-{
-    if (n == 1)
-    {
-        cout<<"Neiother prime nor composite number"<<endl;
-    }
-    
-    for (int i = 2; i * i <= n; i++)
-    {
-        if (n%i == 0)
-        {
-            cout<<"is not a prime number"<<endl;
-        }
-    }
-    cout<<"is a prime number"<<endl;
-}
 
 int main()
 {
-    int n;
-    cout << "n's value - " << endl;
-    cin >> n;
-
-    cout<<isprime(n);
+    
 }
