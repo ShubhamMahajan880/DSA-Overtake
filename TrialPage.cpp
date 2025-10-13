f4760ae0e63e93f7365f592556f4c61eb723688b
Shubham Mahajan <mhajan.shubh@gmail.com>
2025-10-14 00:26:15 +0530

    doing Practise Day & Night

---
+++ TrialPage.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int addition(int a, int b)
+{
+    int c = a + b;
+    return c;
+}
+int main()
+{
+    int a = 10;
+    int b = 5;
+
+    cout << addition(a, b) << endl;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,8 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int addition(int a, int b)
+{
+    int c = a + b;
+    return c;
+}
+int main()
+{
+    int a = 10;
+    int b = 5;
 
-    MatrixRightRotationNinteeDegree(n, m, matrix);
-    printMatrix(n, m, matrix);
-
-    MatrixLeftRotationNinteeDegree(n, m, matrix);
-    printMatrix(n, m, matrix);
+    cout << addition(a, b) << endl;
 }
