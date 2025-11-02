20bace64f10b1a69a1edd97a1801ba273a19db99
Shubham Mahajan <mhajan.shubh@gmail.com>
2025-11-03 02:01:05 +0530

    Pointers and Problem Solving

--- TrialPage.cpp
+++ TrialPage.cpp
@@ -1,65 +1,55 @@
 #include <bits/stdc++.h>
+// #include <iostream>
+// #include<algorithm>
+// #include<climits>
+// #include<string>
+// #include<cctype>
+// #include<vector>
+// #include<set>
 using namespace std;
-
-void MergeSort(int arr[], int st, int end)
-{
-    if (st >= end)
-    {
-        return;
-    }
-    int mid = st + (end - st) / 2;
-    MergeSort(arr, st, mid);
-    MergeSort(arr, mid + 1, end);
-    Merge(arr, st, mid, end);
-}
-void Merge(int arr[], int st, int mid, int end)
-{
-    vector<int> temp;
-    int i = st;
-    int j = mid + 1;
-    if (i <= mid && j <= end)
-    {
-        if (arr[i] <= arr[j])
-        {
-            temp.push_back(arr[i]);
-            i++;
-        }
-        else
-        {
-            temp.push_back(arr[j]);
-            j++;
-        }
-    }
-    while (i <= mid) // For left
-    {
-        temp.push_back(arr[i]);
-        i++;
-    }
-    while (j <= end) //// Similarly for Right -
-    {
-        temp.push_back(arr[j]);
-        j++;
-    }
-    for (int i = st, x = 0; i <= end; i++)
-    {
-        arr[i] = temp[x++]; //// Printing original sorted array via copying elements from temp
-    }
-}
-int printArray(int n, int arr[])
-{
-    cout << "So, the sorted array we got is - " << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
 
 int main()
 {
-    int n = 7;
-    int arr[n] = {23, 56, 8, 12, 45, 78, 99};
+    // Pointers in cpp - Pointers are basically used to store the addrss of a variable in the memory.For all diffrerent datatypes basically different pointers are declared and it stores its value in the memory along with the address.Initialization of a Pointer -
 
-    MergeSort(arr, 0, n - 1);
-    printArray(n, arr);
+    int a = 5;
+    int *ptr = &a;
+    cout << ptr << endl; // 0x61ff08
+
+    // Note - once we've declared the pointer then no need to use * again and again. it can directl beused using the name of pointer fro nextonwards
+
+    int b = 10;
+    int *ptr1;
+    ptr1 = &b;
+    cout << ptr1 << endl; // 0x61ff00
+
+    // by using the pointer name we're here printing the address(&) of that pointer variable
+    //  But foir printing the value inside that pointer we need to use (). : & -> address. of
+    //  But foir printing the value inside that pointer we need to use (*).: * -> value at
+
+    cout << *ptr << endl;  // 5
+    cout << *ptr1 << endl; // 10
+
+    // Double Pointer  - this Approach is  uses for store and print the addrss of a pointer. Means pointer ka pointer. which is Pointer of Pointer
+    int c = 15;
+    int *ptr2 = &a;
+    int **ptr3 = &ptr2;
+    cout << *ptr2 << endl;  // 5
+    cout << ptr2 << endl;   // 0x61fefc
+    cout << *ptr3 << endl;  // 0x61fefc
+    cout << ptr3 << endl;   // 0x61fef4
+    cout << **ptr3 << endl; // 5
+    *ptr2 = 75;
+    cout << **ptr3 << endl; // 75
+
+    // Understanding of pointer with an array -
+    int n = 5;
+    int arr[5] = {2, 4, 6, 8, 10};
+    cout << arr << endl;       // x61fedc - addrss of array stord in the memory
+    cout << (arr + 1) << endl; // 0x61fee0 - address of  any element which we want
+
+    // printing elements throgh pointer in array -
+    cout << *arr << endl;       // 2
+    cout << *(arr + 1) << endl; // 4
 }
+// ____________ ____________ ____________ ____________ ____________
